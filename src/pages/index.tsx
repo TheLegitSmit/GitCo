a3707bc7c2dd3fda83c4531c22905f183412ae50
TheLegitSmit <TheLegitSmit@gmail.com>
2024-02-03 18:33:58 -0800

    update ui2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,8 @@ export default function Chat() {
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <header style={{ fontSize: '2em', marginBottom: '1em' }}>My Chitty Chatty Bot 🤖</header>
       <p style={{ marginBottom: '1em', textAlign: 'center' }}>This is an extremely simple, work-in-progress chatbot. Type your message and press send to interact with it.</p>
-      <p style={{ marginBottom: '1em', textAlign: 'center' }}>No transcripts are saved from the chat. Note that I want to, but I just don't know how yet lol. So no worries about privacy.</p>
-      <p style={{ marginBottom: '1em', textAlign: 'center' }}>Also, the bot doesn't yet have back-and-forth conversational memory yet because I'm still pretty bad at this. 🙃</p>
+      <p style={{ marginBottom: '1em', textAlign: 'center' }}>No transcripts are saved from the chat. Note that I want to, but I just do not know how yet lol. So no worries about privacy.</p>
+      <p style={{ marginBottom: '1em', textAlign: 'center' }}>Also, the bot does not yet have back-and-forth conversational memory yet because I am still pretty bad at this. 🙃</p>
       <div style={{ padding: '1em', backgroundColor: '#fff', borderRadius: '1em', boxShadow: '0 0 10px rgba(0,0,0,0.1)', width: '100%', maxWidth: '600px' }}>
         <div style={{ marginBottom: '1em' }}>
           {messages.map((message, index) => (
